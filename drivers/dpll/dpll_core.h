943547e68aa5d09ba1cb361089392995af25561d
Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
2022-11-19 00:25:15 +0100

    dpll: Add DPLL framework base functions
    DPLL framework is used to represent and configure DPLL devices
    in systems. Each device that has DPLL and can configure sources
    and outputs can use this framework. Netlink interface is used to
    provide configuration data and to receive notification messages
    about changes in the configuration or status of DPLL device.
    Inputs and outputs of the DPLL device are represented as special
    objects which could be dynamically added to and removed from DPLL
    device.
    
    Changes:
    dpll: redesign after ynl and review comments
    
    dpll: replace cookie with clock id
    dpll: add clock class
    
    Provide userspace with clock class value of DPLL with dpll device dump
    netlink request. Clock class is assigned by driver allocating a dpll
    device. Clock class values are defined as specified in:
    ITU-T G.8273.2/Y.1368.2 recommendation.
    
    dpll: follow one naming schema in dpll subsys
    
    dpll: fix dpll device naming scheme
    
    Fix dpll device naming scheme by use of new pattern.
    "dpll_%s_%d_%d", where:
    - %s - dev_name(parent) of parent device,
    - %d (1) - enum value of dpll type,
    - %d (2) - device index provided by parent device.
    
    dpll: remove description length parameter
    
    dpll: fix muxed/shared pin registration
    
    Let the kernel module to register a shared or muxed pin without finding
    it or its parent. Instead use a parent/shared pin description to find
    correct pin internally in dpll_core, simplifing a dpll API.
    
    dpll: move function comments to dpll_core.c, fix exports
    dpll: remove single-use helper functions
    dpll: merge device register with alloc
    dpll: lock and unlock mutex on dpll device release
    dpll: move dpll_type to uapi header
    dpll: rename DPLLA_DUMP_FILTER to DPLLA_FILTER
    dpll: rename dpll_pin_state to dpll_pin_mode
    dpll: rename DPLL_MODE_FORCED to DPLL_MODE_MANUAL
    dpll: remove DPLL_CHANGE_PIN_TYPE enum value
    
    Co-developed-by: Milena Olech <milena.olech@intel.com>
    Signed-off-by: Milena Olech <milena.olech@intel.com>
    Co-developed-by: Michal Michalik <michal.michalik@intel.com>
    Signed-off-by: Michal Michalik <michal.michalik@intel.com>
    Co-developed-by: Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
    Signed-off-by: Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
    Signed-off-by: Vadim Fedorenko <vadfed@fb.com>

---
+++ drivers/dpll/dpll_core.h
@@ -0,0 +1,100 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ *  Copyright (c) 2021 Meta Platforms, Inc. and affiliates
+ */
+
+#ifndef __DPLL_CORE_H__
+#define __DPLL_CORE_H__
+
+#include <linux/dpll.h>
+#include <linux/refcount.h>
+#include "dpll_netlink.h"
+
+#define DPLL_REGISTERED		XA_MARK_1
+#define DPLL_PIN_REGISTERED	XA_MARK_1
+
+/**
+ * struct dpll_device - structure for a DPLL device
+ * @id:		unique id number for each device
+ * @dev:	struct device for this dpll device
+ * @parent:	parent device
+ * @module:	module of creator
+ * @ops:	operations this &dpll_device supports
+ * @lock:	mutex to serialize operations
+ * @type:	type of a dpll
+ * @priv:	pointer to private information of owner
+ * @pins:	list of pointers to pins registered with this dpll
+ * @clock_id:	unique identifier (clock_id) of a dpll
+ * @dev_driver_idx: provided by driver for
+ * @mode_supported_mask: mask of supported modes
+ * @refcount:	refcount
+ **/
+struct dpll_device {
+	u32 id;
+	u32 dev_driver_id;
+	struct device dev;
+	struct device *parent;
+	struct module *module;
+	struct dpll_device_ops *ops;
+	struct mutex lock;
+	enum dpll_type type;
+	void *priv;
+	struct xarray pins;
+	u64 clock_id;
+	unsigned long mode_supported_mask;
+	refcount_t refcount;
+};
+
+/**
+ * struct dpll_pin - structure for a dpll pin
+ * @idx:		unique idx given by alloc on global pin's XA
+ * @dev_driver_id:	id given by dev driver
+ * @clock_id:		clock_id of creator
+ * @type:		type of the pin
+ * @module:		module of creator
+ * @dpll_refs:		hold referencees to dplls that pin is registered with
+ * @pin_refs:		hold references to pins that pin is registered with
+ * @prop:		properties given by registerer
+ * @rclk_dev_name:	holds name of device when pin can recover clock from it
+ * @refcount:		refcount
+ * @lock:		access lock
+ **/
+struct dpll_pin {
+	u32 idx;
+	u32 dev_driver_id;
+	u64 clock_id;
+	struct module *module;
+	struct xarray dpll_refs;
+	struct xarray pin_refs;
+	struct dpll_pin_properties prop;
+	char *rclk_dev_name;
+	refcount_t refcount;
+	struct mutex lock;
+};
+
+struct dpll_pin_ref {
+	union {
+		struct dpll_device *dpll;
+		struct dpll_pin *pin;
+	};
+	struct dpll_pin_ops *ops;
+	void *priv;
+};
+
+struct dpll_device *dpll_device_get_by_id(int id);
+struct dpll_device *dpll_device_get_by_name(const char *bus_name,
+					    const char *dev_name);
+void dpll_device_unregister(struct dpll_device *dpll);
+struct dpll_pin *dpll_pin_get_by_idx(struct dpll_device *dpll, u32 idx);
+struct dpll_pin_ref
+*dpll_pin_find_dpll_ref(const struct dpll_device *dpll,
+			const struct dpll_pin *pin);
+struct dpll_pin_ref
+*dpll_pin_find_pin_ref(const struct dpll_pin *parent,
+		       const struct dpll_pin *pin);
+
+extern struct xarray dpll_device_xa;
+extern struct xarray dpll_pin_xa;
+extern struct mutex dpll_device_xa_lock;
+extern struct mutex dpll_pin_xa_lock;
+#endif

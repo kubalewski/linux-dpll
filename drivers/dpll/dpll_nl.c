28e3c5ef11327e560976991ffb3ee12e11131b51
Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
2023-05-30 10:58:45 +0200

    dpll: spec: rename pin-idx to pin-id, remove pin-parent-idx, device
    dpll device is identified by DPLL_A_ID, now pin is identified with
    similar name DPLL_A_PIN_ID (consistency).
    DPLL_PIN_PARENT_IDX is removed as it is not needed. It was used to
    target a pin parent, but now we will use a DPLL_A_PIN_PARENT nested
    attribute which encloses either DPLL_A_ID or DPLL_A_PIN_ID, depending
    where it is connected. Removed DPLL_A_DEVICE nested attribute as it is
    replaced by DPLL_A_PIN_PARENT.
    
    Signed-off-by: Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>

--- drivers/dpll/dpll_nl.c
+++ drivers/dpll/dpll_nl.c
@@ -10,6 +10,16 @@
 
 #include <linux/dpll.h>
 
+/* Common nested types */
+const struct nla_policy dpll_pin_parent_nl_policy[DPLL_A_PIN_RCLK_DEVICE + 1] = {
+	[DPLL_A_ID] = { .type = NLA_U32, },
+	[DPLL_A_PIN_DIRECTION] = NLA_POLICY_MAX(NLA_U8, 2),
+	[DPLL_A_PIN_PRIO] = { .type = NLA_U32, },
+	[DPLL_A_PIN_STATE] = NLA_POLICY_MAX(NLA_U8, 3),
+	[DPLL_A_PIN_ID] = { .type = NLA_U32, },
+	[DPLL_A_PIN_RCLK_DEVICE] = { .type = NLA_NUL_STRING, },
+};
+
 /* DPLL_CMD_DEVICE_GET - do */
 static const struct nla_policy dpll_device_get_nl_policy[DPLL_A_BUS_NAME + 1] = {
 	[DPLL_A_ID] = { .type = NLA_U32, },
@@ -26,11 +36,11 @@ static const struct nla_policy dpll_device_set_nl_policy[DPLL_A_MODE + 1] = {
 };
 
 /* DPLL_CMD_PIN_GET - do */
-static const struct nla_policy dpll_pin_get_do_nl_policy[DPLL_A_PIN_IDX + 1] = {
+static const struct nla_policy dpll_pin_get_do_nl_policy[DPLL_A_PIN_ID + 1] = {
 	[DPLL_A_ID] = { .type = NLA_U32, },
 	[DPLL_A_BUS_NAME] = { .type = NLA_NUL_STRING, },
 	[DPLL_A_DEV_NAME] = { .type = NLA_NUL_STRING, },
-	[DPLL_A_PIN_IDX] = { .type = NLA_U32, },
+	[DPLL_A_PIN_ID] = { .type = NLA_U32, },
 };
 
 /* DPLL_CMD_PIN_GET - dump */
@@ -41,16 +51,16 @@ static const struct nla_policy dpll_pin_get_dump_nl_policy[DPLL_A_BUS_NAME + 1] 
 };
 
 /* DPLL_CMD_PIN_SET - do */
-static const struct nla_policy dpll_pin_set_nl_policy[DPLL_A_PIN_PARENT_IDX + 1] = {
+static const struct nla_policy dpll_pin_set_nl_policy[DPLL_A_PIN_PARENT + 1] = {
 	[DPLL_A_ID] = { .type = NLA_U32, },
 	[DPLL_A_BUS_NAME] = { .type = NLA_NUL_STRING, },
 	[DPLL_A_DEV_NAME] = { .type = NLA_NUL_STRING, },
-	[DPLL_A_PIN_IDX] = { .type = NLA_U32, },
+	[DPLL_A_PIN_ID] = { .type = NLA_U32, },
 	[DPLL_A_PIN_FREQUENCY] = { .type = NLA_U64, },
 	[DPLL_A_PIN_DIRECTION] = NLA_POLICY_MAX(NLA_U8, 2),
 	[DPLL_A_PIN_PRIO] = { .type = NLA_U32, },
 	[DPLL_A_PIN_STATE] = NLA_POLICY_MAX(NLA_U8, 3),
-	[DPLL_A_PIN_PARENT_IDX] = { .type = NLA_U32, },
+	[DPLL_A_PIN_PARENT] = { .type = NLA_NESTED, },
 };
 
 /* Ops table for dpll */
@@ -86,7 +96,7 @@ static const struct genl_split_ops dpll_nl_ops[] = {
 		.doit		= dpll_nl_pin_get_doit,
 		.post_doit	= dpll_pin_post_doit,
 		.policy		= dpll_pin_get_do_nl_policy,
-		.maxattr	= DPLL_A_PIN_IDX,
+		.maxattr	= DPLL_A_PIN_ID,
 		.flags		= GENL_ADMIN_PERM | GENL_CMD_CAP_DO,
 	},
 	{
@@ -104,7 +114,7 @@ static const struct genl_split_ops dpll_nl_ops[] = {
 		.doit		= dpll_nl_pin_set_doit,
 		.post_doit	= dpll_pin_post_doit,
 		.policy		= dpll_pin_set_nl_policy,
-		.maxattr	= DPLL_A_PIN_PARENT_IDX,
+		.maxattr	= DPLL_A_PIN_PARENT,
 		.flags		= GENL_ADMIN_PERM | GENL_CMD_CAP_DO,
 	},
 };

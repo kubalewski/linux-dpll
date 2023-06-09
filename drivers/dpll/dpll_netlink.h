cf22ef8df857f18d9ea92452b0d14c811941b76c
Vadim Fedorenko <vadim.fedorenko@linux.dev>
2023-06-09 12:09:49 +0200

    dpll: netlink: Add DPLL framework base functions
    DPLL framework is used to represent and configure DPLL devices
    in systems. Each device that has DPLL and can configure inputs
    and outputs can use this framework.
    
    Implement dpll netlink framework functions for enablement of dpll
    subsytem netlink family.
    
    Co-developed-by: Milena Olech <milena.olech@intel.com>
    Signed-off-by: Milena Olech <milena.olech@intel.com>
    Co-developed-by: Michal Michalik <michal.michalik@intel.com>
    Signed-off-by: Michal Michalik <michal.michalik@intel.com>
    Signed-off-by: Vadim Fedorenko <vadim.fedorenko@linux.dev>
    Co-developed-by: Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
    Signed-off-by: Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>

---
+++ drivers/dpll/dpll_netlink.h
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ *  Copyright (c) 2023 Meta Platforms, Inc. and affiliates
+ *  Copyright (c) 2023 Intel and affiliates
+ */
+
+/**
+ * dpll_device_create_ntf - notify that the device has been created
+ * @dpll: registered dpll pointer
+ *
+ * Context: caller shall hold dpll_xa_lock.
+ * Return: 0 if succeeds, error code otherwise.
+ */
+int dpll_device_create_ntf(struct dpll_device *dpll);
+
+/**
+ * dpll_device_delete_ntf - notify that the device has been deleted
+ * @dpll: registered dpll pointer
+ *
+ * Context: caller shall hold dpll_xa_lock.
+ * Return: 0 if succeeds, error code otherwise.
+ */
+int dpll_device_delete_ntf(struct dpll_device *dpll);
+
+/**
+ * dpll_pin_create_ntf - notify that the pin has been created
+ * @pin: registered pin pointer
+ *
+ * Context: caller shall hold dpll_xa_lock.
+ * Return: 0 if succeeds, error code otherwise.
+ */
+int dpll_pin_create_ntf(struct dpll_pin *pin);
+
+/**
+ * dpll_pin_delete_ntf - notify that the pin has been deleted
+ * @pin: registered pin pointer
+ *
+ * Context: caller shall hold dpll_xa_lock.
+ * Return: 0 if succeeds, error code otherwise.
+ */
+int dpll_pin_delete_ntf(struct dpll_pin *pin);
+
+int __init dpll_netlink_init(void);
+void dpll_netlink_finish(void);
